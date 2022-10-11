0751c451641c228c5676e96c84c225d06920c804
AagamShah <shahaagam2314@gmail.com>
2022-10-11 11:30:52 +0530

    Added some front end interfaces
    Added Navbar, Recording interface and scenario interface

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import "./navbar.css";
+import "../styles/reset.css"
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <header class="heading">
+        <span class="fi">learn</span>
+        <span class="se">SBAR</span>
+      </header>
+      <div class="border"></div>
+    </div>
+  );
+};
+export default Navbar;
---
+++ src/components/Record.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./record.css";
+import "../styles/reset.css"
+import Navbar from "./Navbar.js";
+const Record = () => {
+  return (
+    <div>
+      <Navbar />
+      <div>Recording in progress!!!</div>
+    </div>
+  );
+};
+export default Record;
