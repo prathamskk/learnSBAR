0fed7a70f6a20a865ac52291461421d3ae50b3f2
Abhishek Yadav <abhiyd2401@gmail.com>
2022-10-16 20:51:44 +0530

    structured all the questions and options

--- src/components/Reflective.js
+++ src/components/Reflective.js
@@ -15,7 +15,40 @@ const Reflective = () => {
             <div className="quest_1">
               <div>What you did well?</div>
             </div>
-            <div className="options"></div>
+            <div className="options">
+              <div>
+                <input
+                  type="checkbox"
+                  value="Information transfer was clear and in a concise way"
+                />
+                <div>Information transfer was clear and in a concise way</div>
+              </div>
+              <div>
+                <input
+                  type="checkbox"
+                  value="Did not missed any point to convey related to patient"
+                />
+                <div>Did not missed any point to convey related to patient</div>
+              </div>
+              <div>
+                <input
+                  type="checkbox"
+                  value="Emphasized important points while conveying"
+                />
+                <div>Emphasized important points while conveying</div>
+              </div>
+              <div>
+                <input type="checkbox" value="Followed SBAR technique " />
+                <div>Emphasized important points while conveying</div>
+              </div>
+              <div>
+                <input
+                  type="checkbox"
+                  value="Pacing was appropriate (neither too fast or slow)"
+                />
+                <div>Pacing was appropriate (neither too fast or slow)</div>
+              </div>
+            </div>
           </div>
           <div className="reflective_2">
             <div className="quest_1">
