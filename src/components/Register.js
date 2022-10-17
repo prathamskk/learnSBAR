32cf88f3370bfb5baaba814583f8dfdba9440a6a
Abhishek Yadav <abhiyd2401@gmail.com>
2022-10-17 13:44:16 +0530

    changed the position of animation and spinner

--- src/components/Register.js
+++ src/components/Register.js
@@ -129,7 +129,6 @@ const Register = () => {
           ""
         )}
         {SignUpState === "signing up" ? <Spinner /> : ""}
-
         <div className="nurses_gang">
           <img src={nurse_gang} alt="nurses" />
         </div>
@@ -141,199 +140,154 @@ const Register = () => {
           >
             {errMsg}
           </p>
-          <h1>Register</h1>
-          <form onSubmit={handleSubmit} className="rform">
-            <label htmlFor="username">
-              <FontAwesomeIcon
-                icon={faCheck}
-                className={validEmail ? "valid" : "hide"}
-              />
-              <FontAwesomeIcon
-                icon={faTimes}
-                className={validEmail || !email ? "hide" : "invalid"}
-              />
-            </label>
-            <div className="inputContainer">
-              {/* <i className="fa-solid fa-user"></i> */}
-              <input
-                type="text"
-                id="username"
-                placeholder="Enter your valid email address"
-                ref={userRef}
-                autoComplete="off"
-                onChange={(e) => setEmail(e.target.value)}
-                value={email}
-                required
-                aria-invalid={validEmail ? "false" : "true"}
-                aria-describedby="uidnote"
-                onFocus={() => setEmailFocus(true)}
-                onBlur={() => setEmailFocus(false)}
-              />
-
-          <div className="nurses_gang">
-            <img src={nurse_gang} alt="nurses" />
-          </div>
-          <div className="nurse_reg">
-            <p
-              ref={errRef}
-              className={errMsg ? "errmsg" : "offscreen"}
-              aria-live="assertive"
-            >
-              {errMsg}
-            </p>
-            <div className="reg-wrapper">
-              <div className="logo">
-                <img src={logo} alt="learnSBAR" />
-              </div>
-              <h1 className="regi">Register</h1>
-              <form onSubmit={handleSubmit} className="rform">
-                <div className="ui">
-                  <label htmlFor="username" className="verify">
-                    <FontAwesomeIcon
-                      icon={faCheck}
-                      className={validEmail ? "valid" : "hide"}
-                    />
-                    <FontAwesomeIcon
-                      icon={faTimes}
-                      className={validEmail || !email ? "hide" : "invalid"}
-                    />
-                  </label>
-                  <div className="inputContainer">
-                    {/* <i class="fa-solid fa-user"></i> */}
-                    <input
-                      type="text"
-                      id="username"
-                      placeholder="Enter your username"
-                      ref={userRef}
-                      autoComplete="off"
-                      onChange={(e) => setEmail(e.target.value)}
-                      value={email}
-                      required
-                      aria-invalid={validEmail ? "false" : "true"}
-                      aria-describedby="uidnote"
-                      onFocus={() => setEmailFocus(true)}
-                      onBlur={() => setEmailFocus(false)}
-                    />
-
-                    <p
-                      id="uidnote"
-                      className={
-                        emailFocus && email && !validEmail
-                          ? "instructions"
-                          : "offscreen"
-                      }
-                    >
-                      <FontAwesomeIcon icon={faInfoCircle} />
-                      4 to 24 characters.
-                      <br />
-                      Must begin with a letter.
-                      <br />
-                      Letters, numbers, underscores, hyphens allowed.
-                    </p>
-                  </div>
-                </div>
-                <div className="ui">
-                  <label htmlFor="password" className="verify">
-                    <FontAwesomeIcon
-                      icon={faCheck}
-                      className={validPwd ? "valid" : "hide"}
-                    />
-                    <FontAwesomeIcon
-                      icon={faTimes}
-                      className={validPwd || !password ? "hide" : "invalid"}
-                    />
-                  </label>
-                  <div className="inputContainer">
-                    {/* <i class="fa-solid fa-lock"></i> */}
-                    <input
-                      className="password"
-                      type="password"
-                      id="password"
-                      placeholder="Enter your password"
-                      onChange={(e) => setPwd(e.target.value)}
-                      value={password}
-                      required
-                      aria-invalid={validPwd ? "false" : "true"}
-                      aria-describedby="pwdnote"
-                      onFocus={() => setPwdFocus(true)}
-                      onBlur={() => setPwdFocus(false)}
-                    />
-                    <p
-                      id="pwdnote"
-                      className={
-                        pwdFocus && !validPwd ? "instructions" : "offscreen"
-                      }
-                    >
-                      <FontAwesomeIcon icon={faInfoCircle} />
-                      8 to 24 characters.
-                      <br />
-                      Must include uppercase and lowercase letters, a number
-                      <br /> and a special character.
-                      <br />
-                      Allowed special characters:{" "}
-                      <span aria-label="exclamation mark">!</span>{" "}
-                      <span aria-label="at symbol">@</span>{" "}
-                      <span aria-label="hashtag">#</span>{" "}
-                      <span aria-label="dollar sign">$</span>{" "}
-                      <span aria-label="percent">%</span>
-                    </p>
-                  </div>
-                </div>
-
-                <div className="mobile-field">
-                  <div className="ui">
-                    <label htmlFor="mobile_no">
-                      <FontAwesomeIcon
-                        icon={faCheck}
-                        className={validMobile && mobile_no ? "valid" : "hide"}
-                      />
-                      <FontAwesomeIcon
-                        icon={faTimes}
-                        className={
-                          validMobile || !mobile_no ? "hide" : "invalid"
-                        }
-                      />
-                    </label>
-                    {/* <i class="fa-solid fa-mobile"></i> */}
-                    <select name="codes" id="codes" required>
-                      <option value="india">+91</option>
-                      <option value="usa">+1</option>
-                    </select>
-                    <input
-                      type="text"
-                      id="mobile"
-                      onChange={(e) => setMobile(e.target.value)}
-                      value={mobile_no}
-                      required
-                      aria-invalid={validMobile ? "false" : "true"}
-                      onFocus={() => setMobileFocus(true)}
-                      onBlur={() => setMobileFocus(false)}
-                    />
-                  </div>
-                </div>
-                <div className="signup-btn">
-                  <button
-                    className="sign-up"
-                    disabled={!validEmail || !validPwd ? true : false}
-                  >
-                    Sign Up
-                  </button>
-                </div>
-              </form>
-              <br />
-              <br />
-              <br />
-              <p className="aa">
-                Already registered?
-                <br />
-                <span className="line">
-                  <Link to="/login">Login</Link>
-                </span>
-              </p>
+          <div className="reg-wrapper">
+            <div className="logo">
+              <img src={logo} alt="learnSBAR" />
             </div>
+            <h1>Register</h1>
+            <form onSubmit={handleSubmit} className="rform">
+              <div className="ui">
+                <label htmlFor="username" className="verify">
+                  <FontAwesomeIcon
+                    icon={faCheck}
+                    className={validEmail ? "valid" : "hide"}
+                  />
+                  <FontAwesomeIcon
+                    icon={faTimes}
+                    className={validEmail || !email ? "hide" : "invalid"}
+                  />
+                </label>
+                <div className="inputContainer">
+                  {/* <i class="fa-solid fa-user"></i> */}
+                  <input
+                    type="text"
+                    id="username"
+                    placeholder="Enter your username"
+                    ref={userRef}
+                    autoComplete="off"
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                    required
+                    aria-invalid={validEmail ? "false" : "true"}
+                    aria-describedby="uidnote"
+                    onFocus={() => setEmailFocus(true)}
+                    onBlur={() => setEmailFocus(false)}
+                  />
+
+                  <p
+                    id="uidnote"
+                    className={
+                      emailFocus && email && !validEmail
+                        ? "instructions"
+                        : "offscreen"
+                    }
+                  >
+                    <FontAwesomeIcon icon={faInfoCircle} />
+                    4 to 24 characters.
+                    <br />
+                    Must begin with a letter.
+                    <br />
+                    Letters, numbers, underscores, hyphens allowed.
+                  </p>
+                </div>
+              </div>
+              <div className="ui">
+                <label htmlFor="password" className="verify">
+                  <FontAwesomeIcon
+                    icon={faCheck}
+                    className={validPwd ? "valid" : "hide"}
+                  />
+                  <FontAwesomeIcon
+                    icon={faTimes}
+                    className={validPwd || !password ? "hide" : "invalid"}
+                  />
+                </label>
+                <div className="inputContainer">
+                  {/* <i class="fa-solid fa-lock"></i> */}
+                  <input
+                    className="password"
+                    type="password"
+                    id="password"
+                    placeholder="Enter your password"
+                    onChange={(e) => setPwd(e.target.value)}
+                    value={password}
+                    required
+                    aria-invalid={validPwd ? "false" : "true"}
+                    aria-describedby="pwdnote"
+                    onFocus={() => setPwdFocus(true)}
+                    onBlur={() => setPwdFocus(false)}
+                  />
+                  <p
+                    id="pwdnote"
+                    className={
+                      pwdFocus && !validPwd ? "instructions" : "offscreen"
+                    }
+                  >
+                    <FontAwesomeIcon icon={faInfoCircle} />
+                    8 to 24 characters.
+                    <br />
+                    Must include uppercase and lowercase letters, a number
+                    <br /> and a special character.
+                    <br />
+                    Allowed special characters:{" "}
+                    <span aria-label="exclamation mark">!</span>{" "}
+                    <span aria-label="at symbol">@</span>{" "}
+                    <span aria-label="hashtag">#</span>{" "}
+                    <span aria-label="dollar sign">$</span>{" "}
+                    <span aria-label="percent">%</span>
+                  </p>
+                </div>
+              </div>
+
+              <div className="mobile-field">
+                <div className="ui">
+                  <label htmlFor="mobile_no">
+                    <FontAwesomeIcon
+                      icon={faCheck}
+                      className={validMobile && mobile_no ? "valid" : "hide"}
+                    />
+                    <FontAwesomeIcon
+                      icon={faTimes}
+                      className={validMobile || !mobile_no ? "hide" : "invalid"}
+                    />
+                  </label>
+                  {/* <i class="fa-solid fa-mobile"></i> */}
+                  <select name="codes" id="codes" required>
+                    <option value="india">+91</option>
+                    <option value="usa">+1</option>
+                  </select>
+                  <input
+                    type="text"
+                    id="mobile"
+                    onChange={(e) => setMobile(e.target.value)}
+                    value={mobile_no}
+                    required
+                    aria-invalid={validMobile ? "false" : "true"}
+                    onFocus={() => setMobileFocus(true)}
+                    onBlur={() => setMobileFocus(false)}
+                  />
+                </div>
+              </div>
+              <div className="signup-btn">
+                <button
+                  className="sign-up"
+                  disabled={!validEmail || !validPwd ? true : false}
+                >
+                  Sign Up
+                </button>
+              </div>
+            </form>
           </div>
-        </section>
-      )}
+
+          <p className="aa">
+            Already registered?
+            <br />
+            <span className="line">
+              <Link to="/login">Login</Link>
+            </span>
+          </p>
+        </div>
+      </section>
     </>
   );
 };
